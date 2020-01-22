60a8884ce5810ce0e556b62e7bab577e5e892acb
aaku14n <akarshgdps@gmail.com>
2020-01-22 23:22:44 +0530

    Adding setup

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,29 @@
 import React, { useState } from "react";
 import { Text, View, Image, TouchableHighlight } from "react-native";
-
-export default class Search extends React.Component {
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+class SearchComponent extends React.Component {
   render() {
     return (
       <View>
         <Text>Search</Text>
+        <Text
+          style={{ margin: 20, fontSize: 20 }}
+          onPress={() => this.props.navigation.navigate("HomeScreen")}
+        >
+          Click here
+        </Text>
       </View>
     );
   }
 }
+function MyNotificationsScreen() {
+  return <Text>Natifiaction </Text>;
+}
+const AppNavigator = createStackNavigator({
+  SettingScreen: { screen: SearchComponent },
+  HomeScreen: { screen: MyNotificationsScreen }
+});
+
+// const Search = createAppContainer(MyDrawerNavigator);
+export default AppNavigator;
